a09844baaa7b3a5943dc968543da35dd995510ff
smz1680@gmail.com <smz1680@gmail.com>
2020-04-09 16:31:37 -0700

    add ting filter to js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $('#photo-gallery').lightGallery(); 
 
 const $searchBar = $(".search-input");
-const $imgs = $("img");
+const $imgs = $("img")
+
+$searchBar.on("keyup",function(){
+    const filterValue = $searchBar.val().toLowerCase(); 
+    $imgs.parent().show();
+    for(let i = 0; i < $imgs.length; i += 1){
+        var titles = $imgs[i].getAttribute("title");
+        if(titles.includes(filterValue)){
+            $imgs.not(`[title*="${filterValue}"]`).parent().hide();
+        }
+    }
+})
