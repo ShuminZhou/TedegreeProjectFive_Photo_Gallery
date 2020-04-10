1a60cf02a657de66aef7352e5520dd15e5215cdd
smz1680@gmail.com <smz1680@gmail.com>
2020-04-09 20:25:28 -0700

    add filter bar functional

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 $('#photo-gallery').lightGallery(); 
 
 const $searchBar = $(".search-input");
-const $imgs = $("img")
+const $imgs = $("img");
 
 $searchBar.on("keyup",function(){
-    const filterValue = $searchBar.val().toLowerCase(); 
-    $imgs.parent().show();
-    for(let i = 0; i < $imgs.length; i += 1){
-        var titles = $imgs[i].getAttribute("title");
-        if(titles.includes(filterValue)){
-            $imgs.not(`[title*="${filterValue}"]`).parent().hide();
+    let $filterValue = $searchBar.val().toLowerCase();
+    for(i = 0 ;i < $imgs.length;i += 1){
+        let titles = $imgs[i].getAttribute("title");
+        if(titles.indexOf($filterValue) > -1){
+            $imgs[i].parentNode.style.display = "";
+        }else{
+            $imgs[i].parentNode.style.display = "none";
         }
     }
 })
